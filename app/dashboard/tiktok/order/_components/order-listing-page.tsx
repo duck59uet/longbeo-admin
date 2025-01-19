dc476f472376ea0fdfd22b6ba42124fb8398e262
duck59uet <ducnv1911@gmail.com>
2025-01-19 14:04:42 +0700

    feat: tiktok

---
+++ app/dashboard/tiktok/order/_components/order-listing-page.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import PageContainer from '@/components/layout/page-container';
+import { Card } from '@/components/ui/card';
+import { useEffect, useState } from 'react';
+import { DataTable as OrderTable } from '@/components/ui/table/data-table';
+import { columns } from './columns';
+import { getOrdersHistory } from '@/services/order';
+
+export default function OrderHistoryPage() {
+  const [data, setData] = useState([]);
+  const [totalItems, setTotalItems] = useState(0);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+
+  const fetchData = async (page: any, limit: any) => {
+    try {
+      const result = await getOrdersHistory({ categoryId: 5, page, limit });
+      setData(result.Data[1]);
+      setTotalItems(result.total[0]);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(page, limit);
+  }, [page, limit]);
+
+  const handlePageChange = (newPage: any) => {
+    setPage(newPage);
+  };
+
+  const handleLimitChange = (newLimit: any) => {
+    setLimit(newLimit);
+  };
+
+  // console.log(data);
+  return (
+    <PageContainer scrollable>
+      <div className="space-y-2">
+        <div className="grid gap-4">
+          <Card>
+            <OrderTable
+              columns={columns}
+              data={data}
+              totalItems={totalItems}
+              onPageChange={handlePageChange}
+              onLimitChange={handleLimitChange}
+            />
+          </Card>
+        </div>
+      </div>
+    </PageContainer>
+  );
+}
---
+++ app/dashboard/tiktok/service/_components/cell-action.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { Employee } from '@/constants/data';
+import { Edit, MoreHorizontal, Trash } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { UpdateServiceModal } from './update-dialog';
+
+interface CellActionProps {
+  data: Employee;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const onConfirm = async () => {};
+
+  return (
+    <>
+      <UpdateServiceModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onConfirm}
+        loading={loading}
+        data={data}
+      />
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Tháo tác</DropdownMenuLabel>
+
+          <DropdownMenuItem
+            onClick={() => setOpen(true)}
+          >
+            <Edit className="mr-2 h-4 w-4" /> Chỉnh sửa
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
