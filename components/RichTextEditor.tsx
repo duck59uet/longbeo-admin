356109fa9d25371ad0a44b7b744f3829ef3de5ce
duck59uet <ducnv1911@gmail.com>
2025-03-16 22:00:55 +0700

    fix

--- components/RichTextEditor.tsx
+++ components/RichTextEditor.tsx
@@ -83,18 +83,41 @@ const RichTextEditor: React.FC<RichTextEditorProps> = ({ value, onChange, placeh
     onChange(content);
   };
 
-  // Cấu hình toolbar của Quill
+  // Định nghĩa bộ màu cơ bản
+  const colors = [
+    '#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', 
+    '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', 
+    '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', 
+    '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', 
+    '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466'
+  ];
+
+  // Cấu hình toolbar của Quill với chỉ thêm tùy chọn màu chữ
   const modules = {
-    toolbar: [
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-      ['bold', 'italic', 'underline', 'strike'],
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      [{ 'indent': '-1'}, { 'indent': '+1' }],
-      [{ 'align': [] }],
-      ['link', 'image'],
-      ['clean']
-    ],
+    toolbar: {
+      container: [
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        [{ 'color': colors }], // Chỉ thêm tùy chọn màu chữ
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'indent': '-1'}, { 'indent': '+1' }],
+        [{ 'align': [] }],
+        ['link', 'image'],
+        ['clean']
+      ]
+    }
   };
+
+  // Định nghĩa các định dạng được hỗ trợ
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike',
+    'color', // Chỉ bao gồm màu chữ, không có background
+    'list', 'bullet',
+    'indent',
+    'align',
+    'link', 'image',
+  ];
 
   if (!isClient) {
     return <div className="quill-loading">Loading editor...</div>;
@@ -107,6 +130,7 @@ const RichTextEditor: React.FC<RichTextEditorProps> = ({ value, onChange, placeh
         value={editorValue}
         onChange={handleChange}
         modules={modules}
+        formats={formats}
         placeholder={placeholder || 'Nhập nội dung...'}
       />
     </div>
