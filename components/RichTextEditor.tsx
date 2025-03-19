c7e01e550e4252be66aafdefb02134c2108620b5
duck59uet <ducnv1911@gmail.com>
2025-03-19 11:11:10 +0700

    fix

--- components/RichTextEditor.tsx
+++ components/RichTextEditor.tsx
@@ -129,6 +129,7 @@ const RichTextEditor: React.FC<RichTextEditorProps> = ({ value, onChange, placeh
           { font: Font.whitelist }, 
           { size: Size.whitelist }
         ],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
         ['bold', 'italic', 'underline', 'strike'],
         [{ color: colors }],
         [{ list: 'ordered' }, { list: 'bullet' }],
